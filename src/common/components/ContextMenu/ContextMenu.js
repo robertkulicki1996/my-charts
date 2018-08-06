0fa65e89d6f60a51384290ef2b1bc689ff6d626a
robku96 <robertkulicki1@wp.pl>
2018-08-06 21:41:11 +0200

    context menu component

---
+++ src/common/components/ContextMenu/ContextMenu.js
@@ -0,0 +1,76 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import { observable, action } from 'mobx';
+import { observer } from 'mobx-react';
+import onClickOutside from 'react-onclickoutside';
+
+import Button from '../Button/Button';
+
+import './ContextMenu.scss';
+
+@observer
+class ContextMenu extends Component {
+  static propTypes = {
+    /**
+     * Position of context menu
+     */
+    position: PropTypes.oneOf(['rightTop', 'rightBottom']),
+    /**
+     * Body of context menu
+     */
+    body: PropTypes.node,
+    /**
+     * Children of context menu - elements that trigger context menu
+     */
+    children: PropTypes.node
+  }
+
+  static defaultProps = {
+    body: null,
+    position: 'rightTop',
+  };
+
+  @observable visible;
+
+  constructor(props) {
+    super(props);
+    this.visible = false;
+  }
+
+  @action.bound
+  handleTriggerClick() {
+    this.visible = !this.visible;
+  }
+
+  @action.bound
+  handleClickOutside() {
+    this.visible = false;
+  }
+
+  render() {
+    const { position, body, children } = this.props;
+
+    return (
+      <div className="context-menu-container">
+        <Button
+          buttonStyle='button-link'
+          tabIndex={0}
+          className='trigger'
+          onClick={this.handleTriggerClick}
+        >
+          {children}
+        </Button>
+        <div
+          onClick={this.handleTriggerClick}
+          tabIndex={0}
+          role="button"
+          className={`context-menu js-menu ${position} ${this.visible ? 'context-menu--is-shown' : '' }`}
+        >
+          {body}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default onClickOutside(ContextMenu);
--- src/common/components/Header/Header.js
+++ src/common/components/Header/Header.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { observer } from 'mobx-react';
 
 import AppLogoIcon from '../../icons/logo.svg';
 import UserAvatar from '../../icons/UserAvatar.svg';
+
 import './Header.scss';
 
+import ContextMenu from '../ContextMenu/ContextMenu';
+
+@observer
 export default class Header extends Component {
   static propTypes = {
     leftSide: PropTypes.node
@@ -13,12 +18,16 @@ export default class Header extends Component {
   static defaultProps = {
     leftSide: null
   };
+  
 
   render() {
+  
     return (
       <div className="header">
         <AppLogoIcon width={48} height={48} />
-        <UserAvatar width={54} height={54} onClick={() => console.log("clicked")}/>
+        <ContextMenu body={<div><div>Menu item 1</div><div>Menu item 1</div></div>} position="rightTop">
+          <UserAvatar className="user-avatar" width={48} height={48} />
+        </ContextMenu>
       </div>
     );
   }
