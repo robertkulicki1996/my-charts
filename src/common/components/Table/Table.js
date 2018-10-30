3fb4b346882dbfc4963caaee6307be4ea6ea7952
robku96 <robertkulicki1@wp.pl>
2018-10-31 00:50:44 +0100

    table update

--- src/common/components/Table/Table.js
+++ src/common/components/Table/Table.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 import { observer } from 'mobx-react';
+import { remove, includes } from 'lodash';
 import { Bind } from 'lodash-decorators';
-import { observable, action } from 'mobx';
+import { observable, action, runInAction } from 'mobx';
 
 // icons
 import DeleteIcon from '../../icons/delete.svg';
+
+// components
+import Button from '../Button/Button';
 
 // styles
 import './Table.scss';
@@ -20,7 +24,8 @@ export default class Table extends Component {
     'Company',
     'Contact',
     'Field',
-    'Country'
+    'Country',
+    'Field'
   ];
 
   @observable rows = [
@@ -28,37 +33,50 @@ export default class Table extends Component {
       'Alfreds Futterkiste',
       'Maria Anders',
       'Germany',
-      'Maria Anders'
+      'Maria Anders',
+      'Alfreds Futterkiste',
     ],
     [
       'Alfreds Futterkiste',
       'Maria Anders',
       'Germany',
-      'Maria Anders'
+      'Maria Anders',
+      'Alfreds Futterkiste',
     ],
     [
       'Alfreds Futterkiste',
       'Maria Anders',
       'Germany',
-      'Maria Anders'
+      'Maria Anders',
+      'Alfreds Futterkiste',
     ],
     [
       'Alfreds Futterkiste',
       'Maria Anders',
       'Germany',
-      'Maria Anders'
+      'Maria Anders',
+      'Alfreds Futterkiste',
     ],
     [
       'Alfreds Futterkiste',
       'Maria Anders',
       'Germany',
-      'Maria Anders'
+      'Maria Anders',
+      'Alfreds Futterkiste',
     ],
   ];
 
+  @observable existingIndexes = [];
+
+  componentDidMount() {
+    runInAction(() => {
+      this.existingIndexes = Array.from(new Array(this.rows.length),(val,index) => index);
+    })
+  }
+
   @action.bound
   addRow(){
-    const { columns, rows } = this;
+    const { columns, rows, existingIndexes } = this;
     let columnsCount = columns.length;
     const newRowData = [];
     while(columnsCount > 0) {
@@ -66,24 +84,47 @@ export default class Table extends Component {
       columnsCount--;
     }
     rows.push(newRowData);
+    existingIndexes.push(existingIndexes.length);
+
+    console.log(existingIndexes);
   }
 
-  @Bind()
-  showIndex(x) {
-    console.log(x);
+  @action.bound
+  removeRow(index) {
+    const { rows, existingIndexes } = this;
+    remove(this.rows[index]);
+    // rows.splice(rows.indexOf(index),1);
+    // console.log(index);
+    // console.log(this.rows);
+    // console.log(this.existingIndexes);
+    existingIndexes.splice(existingIndexes.indexOf(index),1);
   }
-
 
   render() {
-    const { columns, rows } = this;
+    const { columns, rows, existingIndexes } = this;
+
+    const removeRowButton = (index) => (
+      <Button key={index} className="remove-button" onClick={() => this.removeRow(index)}>
+        <DeleteIcon 
+          className="remove-button"  
+          width={14} 
+          height={14} 
+        />
+      </Button>
+    );
+
     return (
       <table>
         <thead>
-          <tr>{columns.map(column => (<th>{column}</th>))}</tr>
+          <tr>{columns.map((column, index)=> (<th key={index}>{column}</th>))}</tr>
         </thead>
         <tbody>
-          {rows.map((row) => (<tr>{row.map((cell) => (<td onClick={this.showIndex(this)}>{cell}</td>))}
-            <td><DeleteIcon className="remove-button" width={14} height={14} /></td>
+          {rows.map((row,index) => (<tr key={index}>{row.map((cell,index) => (<td key={index}>{cell}</td>))}
+            {includes(existingIndexes, index) && (
+              <td key={index}>
+                {removeRowButton(index)}
+              </td>
+            )}
           </tr>))}
         </tbody>
       </table>
